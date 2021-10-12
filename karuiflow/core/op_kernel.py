fix(matmul kernel op): gradient computation refactor(kernel op): outer gradients is required for computing the corresponding gradients refactor(AbstractTensor): added zero_grad method

karuiflow/core/op_kernel.py
@@ -9,5 +9,5 @@ class OpKernel:
         pass
 
     @abstractmethod
-    def backward(self, input_values: List[np.ndarray]) -> List[np.ndarray]:
+    def backward(self, input_values: List[np.ndarray], outer_grad: np.ndarray) -> List[np.ndarray]:
         pass

karuiflow/core/tensor.py
@@ -45,8 +45,14 @@ class Tensor(AbstractTensor):
             return
         else:
             input_vals = [x.data for x in self.input_tensors]
-            grads = self.op.backward(input_vals)
+            grads = self.op.backward(input_vals, outer_grad)
 
         for grad, tensor in zip(grads, self.input_tensors):
             # Invoke computation of the inner gradients
-            tensor.backward(np.dot(outer_grad, grad.T))
+            tensor.backward(grad)
+
+    def zero_grad(self):
+        self.grad = np.zeros_like(self.grad)
+
+        for x in self.input_tensors:
+            x.zero_grad()

karuiflow/ops/matmul/kernel.py
@@ -7,5 +7,11 @@ class MatMulKernel(OpKernel):
     def forward(self, input_vals):
         return np.dot(input_vals[0], input_vals[1])
 
-    def backward(self, input_vals):
-        return [input_vals[1].T, input_vals[0].T]
+    def backward(self, input_vals, outer_grad):
+        grad0 = input_vals[1]
+        grad0 = np.dot(outer_grad, grad0.T)
+
+        grad1 = input_vals[0]
+        grad1 = np.dot(grad1.T, outer_grad)
+        return [grad0, grad1]
+
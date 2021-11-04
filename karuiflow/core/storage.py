8b4887af8c73e61ad68c2d68d115b42d4c508453
oKatanaaa <whitemarsstudios@gmail.com>
2021-11-04 23:06:53 +0400

    refactor: updated readme

--- karuiflow/core/storage.py
+++ karuiflow/core/storage.py
@@ -1,4 +1,19 @@
+from abc import abstractmethod
 import numpy as np
+
+
+class Device:
+    """
+    To make the storage independent of the specifics of memory allocation on different devices,
+    we use this instance for allocation on the given device.
+    """
+    @abstractmethod
+    def allocate_memory(self, nbytes):
+        pass
+
+    @abstractmethod
+    def deallocate_memory(self, data):
+        pass
 
 
 class Storage:
@@ -7,18 +22,23 @@ class Storage:
     Since we don't want to tie all this hustle with computational logic (Tensor, Operations), all the memory
     business is encapsulated within this class.
     """
-    def __init__(self, dtype, shape, device, data=None):
+    def __init__(self, dtype, shape, device: Device):
         self._dtype = dtype
         self._shape = shape
         self._device = device
-        self._data = data
+        self._data = None
 
-        if data is None:
-            self.allocate_memory()
+        self.initialize()
 
-    def allocate_memory(self):
+    def initialize(self):
+        # nbytes = compute number of needed bytes...
+        # self._data = self._device.allocate_memory(nbytes)
         self._data = np.empty(shape=self._shape, dtype=self._dtype)
 
     def deallocate_memory(self):
+        self._device.deallocate_memory(self._data)
+
+    def copy_memory(self, storage):
+        # How to copy memory from device to device?
         pass
 
